44989f6504cc34525b9cddb8f55496816e85f515
BrunoMSPais <bpais@outlook.pt>
2023-09-11 01:20:19 +0100

    project setup

---
+++ styles/globals.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1b1b1b',
+    paddingVertical: 40,
+    paddingHorizontal: 10
+  },
+  titleText: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: '600'
+  }
+})
